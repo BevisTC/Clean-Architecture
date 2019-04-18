6b423875e4a2ac13fc542d2a34e18ae1202b1fa0
bevis <bevis@binodata.com>
2019-04-19 01:31:05 +0800

    init db project

---
+++ CourseManagement/DB/CourseManagement/dbo/tables/Course.sql
@@ -0,0 +1,56 @@
+﻿CREATE TABLE [dbo].[Course]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[Name] NVARCHAR(50) NOT NULL, 
+    [Description] NVARCHAR(MAX) NULL, 
+    [Price] INT NULL, 
+    [Memo] NVARCHAR(MAX) NULL,
+
+
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'課程說明',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Course',
+    @level2type = N'COLUMN',
+    @level2name = N'Description'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'課程名稱',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Course',
+    @level2type = N'COLUMN',
+    @level2name = N'Name'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'識別碼',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Course',
+    @level2type = N'COLUMN',
+    @level2name = N'Id'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'定價',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Course',
+    @level2type = N'COLUMN',
+    @level2name = N'Price'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'備註',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Course',
+    @level2type = N'COLUMN',
+    @level2name = N'Memo'
